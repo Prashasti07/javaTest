62b49ad5688cb2af84a14b6307c288e1fdc7e614
Prashasti Porwal <porwal_p@India.XoriantCorp.com>
2019-09-25 15:19:30 +0530

    adding project to git

---
+++ src/main/java/com/java/test/javaTest/App.java
@@ -0,0 +1,13 @@
+package com.java.test.javaTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

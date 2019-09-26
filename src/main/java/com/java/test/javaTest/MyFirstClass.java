fdffe79b494567b00874b959113900316fd9bd2e
Prashasti07 <prashastiporwal07@gmail.com>
2019-09-26 23:22:27 +0530

    add

--- src/main/java/com/java/test/javaTest/MyFirstClass.java
+++ src/main/java/com/java/test/javaTest/MyFirstClass.java
@@ -6,11 +6,11 @@ public class MyFirstClass {
 	System.out.println("First Class");
 	System.out.println("Test");
 	System.out.println("New branch commit");
-	System.out.println("Feature branch");
+
 	System.out.println("Master branch conflict");
 	System.out.println("Learn merge conflict by tool");
 	System.out.println("New line added");
-    System.out.println("Akash Jain");
+    System.out.println("Akash Jain123");
 	System.out.println("Prashasti Porwal");
 	}
 

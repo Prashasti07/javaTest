8c01a1f12fc5a8c5a23906eca5ec8950d50a776a
Prashasti07 <41124342+Prashasti07@users.noreply.github.com>
2019-09-29 20:41:27 +0530

    Update Test.java

--- src/main/java/com/java/test/javaTest/Test.java
+++ src/main/java/com/java/test/javaTest/Test.java
@@ -14,18 +14,3 @@ public class Test {
 	}
 
 }
-
-package com.java.test.javaTest;
-
-public class Test {
-
-	public static void main(String[] args) {
-		int a=20, b=30;
-		int c=a+b;
-		
-		System.out.println("Sum is ====>  "+ c);
-
-	}
-
-}
-

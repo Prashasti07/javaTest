701b8e23e1a7aabf31223d3bce5f98c912ca05f4
Prashasti07 <41124342+Prashasti07@users.noreply.github.com>
2019-09-29 20:42:59 +0530

    Update Test.java

--- src/main/java/com/java/test/javaTest/Test.java
+++ src/main/java/com/java/test/javaTest/Test.java
@@ -4,10 +4,6 @@ package com.java.test.javaTest;
 public class Test {
 
 	public static void main(String[] args) {
-		int a=20, b=30;
-		int c=a+b;
-		
-		System.out.println("Sum is ====>  "+ c);
 		int a=10, b=20;
 		System.out.println("Addition of a & b is :  ====>  "+ (a+b));		
 
